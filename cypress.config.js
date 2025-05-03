6e8738b61fda7bf6940ed56a45e9a559a474238b
Maarjorieee <marjorie_nascimento@hotmail.com>
2025-05-03 19:36:44 -0300

    remocao do cypress config do gitignore

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "icytgo",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    video:true,
+    reporter:'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: false,
+      timestamp: 'yyyy-mm-dd_HH-MM-ss',
+    },
+  },
+});
