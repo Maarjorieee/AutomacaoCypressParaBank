c2c05d1ebf683e130a0fe4fb33f036ee1d52313b
Maarjorieee <marjorie_nascimento@hotmail.com>
2025-08-17 18:44:17 -0300

    Commit do projeto atualizado

---
+++ cypress/e2e/users-actions.cy.js
@@ -0,0 +1,36 @@
+import { users } from "../fixtures/user-data-testing.json";
+import { usersLogin } from "../fixtures/user-data-login.json";
+
+describe("User's Actions", () => {
+  beforeEach(() => {
+    cy.visit("https://parabank.parasoft.com/parabank/index.htm");
+  });
+
+  users.forEach((user) => {
+    it(`Should register a new user: ${user.firstName} ${user.lastName}`, () => {
+      cy.userInformationRegister(
+        user.firstName,
+        user.lastName,
+        user.streetName,
+        user.cityName,
+        user.stateName,
+        user.zipCode,
+        user.phoneNumber,
+        user.ssn
+      );
+      cy.userCreation(user.userName, user.password);
+      cy.userCreationConfirmationMessage(user.userName);
+    });
+  });
+
+  usersLogin.forEach((user) => {
+    it("Should deposit money in the user's account", () => {
+      cy.userLoginIn(user.userName, user.password);
+      cy.transferingFundsFromCheckingToSavings();
+    });
+  });
+
+  it("Reset database", () => {
+    cy.resetData();
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -50,28 +50,8 @@ Cypress.Commands.add("transferingFundsFromCheckingToSavings", () => {
   cy.get("tr > :nth-child(2) > a").click();
 });
 
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("resetData", () => {
+  cy.get('#headerPanel > ul.leftmenu > li:nth-child(6) > a').click();
+  cy.contains('button', 'Clean').click();  
+  cy.get('#rightPanel > p > b').should('have.text', 'Database Cleaned');
+})
